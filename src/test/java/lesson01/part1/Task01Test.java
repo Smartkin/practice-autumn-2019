13907378085095c55eebe21d20634ec2c0be0b82
Smartkin <vladislav5019@gmail.com>
2019-10-14 23:24:32 +0300

    Fix sudden merge issues, create first unit test

---
+++ src/test/java/lesson01/part1/Task01Test.java
@@ -0,0 +1,24 @@
+package lesson01.part1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import util.SystemOutGatewayUtil;
+
+import java.io.ByteArrayOutputStream;
+
+import static org.junit.Assert.*;
+
+@RunWith(JUnit4.class)
+public class Task01Test {
+
+    @Test
+    public void Test01(){
+        SystemOutGatewayUtil.setCustomOut();
+        Task01.main(null);
+        ByteArrayOutputStream outputStream = SystemOutGatewayUtil.getOutputArray();
+        String cleanStr = outputStream.toString().replaceAll("\r\n", "");
+        assertTrue(cleanStr.equals("Hello World!"));
+    }
+
+}
