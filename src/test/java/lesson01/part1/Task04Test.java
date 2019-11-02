b377ad8eaea9c7b6210cfc52dc9dc63c75533141
Smartkin <vladislav5019@gmail.com>
2019-11-02 12:56:25 +0300

    Finish applying all changes from lesson02 branch

--- src/test/java/lesson01/part1/Task04Test.java
+++ src/test/java/lesson01/part1/Task04Test.java
@@ -1,6 +1,10 @@
 package lesson01.part1;
 
 import org.junit.Test;
+import util.SystemOutGatewayUtil;
+
+import java.io.*;
+import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
@@ -8,5 +12,35 @@ public class Task04Test {
 
     @Test
     public void main() {
+        try{
+            BufferedReader br = new BufferedReader(new FileReader("D:\\Programming\\GitHub-repos\\practice-autumn-2019\\src\\main\\java\\lesson01\\part1\\Task04.java"));
+            SystemOutGatewayUtil.setCustomOut();
+            Task04.main(null);
+            ByteArrayOutputStream outputStream = SystemOutGatewayUtil.getOutputArray();
+            String[] answers = outputStream.toString().replaceAll("\r","").split("\n");
+            int numLines = 0;
+            int numSlashes = 0;
+            String str;
+            while((str = br.readLine()) != null){
+                numLines++;
+                while(str.matches(".*/.*")){
+                    numSlashes++;
+                    str = str.replaceFirst("/", "");
+                }
+            }
+            assertEquals(33, numLines);
+            assertTrue(numSlashes == 16);
+            assertTrue(answers[0].equals("26"));
+            assertTrue(answers[1].equals("25"));
+        }
+        catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+            System.out.println(Arrays.toString(e.getStackTrace()));
+        }
+        catch(IOException e){
+            System.out.println(e.getMessage());
+            System.out.println(Arrays.toString(e.getStackTrace()));
+        }
+
     }
 }
